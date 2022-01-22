431a8a75b8ae176f0ec160064651c16246b66465
ZM <zee.mun.uno@gmail.com>
2022-01-22 16:45:54 +0800

    Handle Search Submit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
 const App = () => {
+  const [word, setWord] = useState('');
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  }
+
+  console.log(word);
+  
   return (
     <div>
       <Header title="Images Gallery"/>
-      <Search />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
     </div>
   )
 }
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Container, Row, Col, Button, Form } from "react-bootstrap";
 
-const Search = () => {
+const Search = ( {word, setWord, handleSubmit} ) => {
   return (
     <Container className="mt-4">
       <Row className="justify-content-center">
         <Col xs={12} md={8} lg={6}>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Row>
               <Col xs={9}>
-                <Form.Control placeholder="Search for new image..." />
+                <Form.Control 
+                    type="text"
+                    value={word}
+                    onChange= {(e) => setWord(e.target.value)}
+                    placeholder="Search for new image..." />
               </Col>
               <Col>
                 <Button variant="primary" type="submit">Search</Button>
