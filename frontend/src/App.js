2e0639d591be5e322f532de38966e66afb252fdd
ZM <zee.mun.uno@gmail.com>
2022-01-22 15:43:28 +0800

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Search from './components/Search';
+
+const App = () => {
+  return (
+    <div>
+      <Header title="Images Gallery"/>
+      <Search />
+    </div>
+  )
+}
+
+export default App;
